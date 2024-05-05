1567d2bf9822f4fe3791104f8c6e3105595b76b8
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2024-05-06 00:45:00 +0300

    .

---
+++ Assignment 3/Assets/_Project/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject obstacle;
+
+
+
+}
